b7ac2d7b2166a97baeeaf9d0566f1bf7f6881411
muammaralikh <muammaralikh@gmail.com>
2026-05-04 16:09:57 +0700

    Remove pagination controls from ongkir admin page

--- resources/views/admin/pages/profil-toko.blade.php
+++ resources/views/admin/pages/profil-toko.blade.php
@@ -48,14 +48,6 @@
                     <h3 class="card-title">Tarif Ongkir per Kecamatan/Kota</h3>
                 </div>
                 <div class="card-body">
-                    <style>
-                        /* Hide table summary/info text on Ongkir page pagination */
-                        .dataTables_info,
-                        .pagination-info,
-                        .page-info {
-                            display: none !important;
-                        }
-                    </style>
                     <form method="GET" action="{{ route('profil-toko.index') }}" class="mb-4">
                         <div class="row">
                             <div class="col-md-5 col-sm-8 mb-2">
@@ -109,12 +101,6 @@
                             </tbody>
                         </table>
                     </div>
-
-                    @if($shippingRates->hasPages())
-                        <div class="mt-3">
-                            {{ $shippingRates->links() }}
-                        </div>
-                    @endif
                 </div>
             </div>
         </div>
